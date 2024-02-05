6e16919fac1fff2be16f7f9da533204ade7afc12
RobertSarsoza <sarsoza@gmail.com>
2024-02-05 10:43:34 -0800

    backend complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+cats = [
+  {
+    name: 'Soza',
+    age: 2,
+    enjoys: 'Long naps on the couch, and a warm fire.',
+    image: 'http://tinyurl.com/cozy-fire-cat'
+  },
+  {
+    name: 'Hiro',
+    age: 12,
+    enjoys: 'Food mostly, really just food.',
+    image: 'http://tinyurl.com/anime-cat-food'
+  },
+  {
+    name: 'Andrew',
+    age: 5,
+    enjoys: 'Kung Fu fighting.',
+    image: 'http://tinyurl.com/andrew-cat-fight'
+  },
+  {
+    name: 'Alex',
+    age: 2,
+    enjoys: 'Scratching on carpet.',
+    image: 'http://tinyurl.com/alex-scratch-cat'
+  }
+]
+
+cats.each do |each_cat|
+  Cat.create each_cat
+  puts "creating cat #{each_cat}"
+end
