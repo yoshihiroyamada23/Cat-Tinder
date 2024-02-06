46220befc818ae3fa6f0af7f0bf5c4ca54a76d99
AndrewBalink <andrew.balink@gmail.com>
2024-02-05 16:59:47 -0800

    final fix api endpoints

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,18 +1,18 @@
 class CatsController < ApplicationController
 
-def index
-    cats = Cat.all
-    render json: cats
-end
+    def index
+        cats = Cat.all
+        render json: cats
+    end
       
-def create
+    def create
     # Create a new cat
-    cat = Cat.create(cat_params)
-    render json: cat
-end
+        cat = Cat.create(cat_params)
+        render json: cat
+    end
 
-private
-def cat_params
-    params.require(:cat).permit(:name, :age, :enjoys, :image)
-end   
-end
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :age, :enjoys, :image)
+    end   
+    end
