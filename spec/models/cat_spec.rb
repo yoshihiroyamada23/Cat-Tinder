051b2e2c65284f2eff090d34611f0fc3943d45db
RobertSarsoza <sarsoza@gmail.com>
2024-02-06 12:13:43 -0800

    validations complete

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is a valid cat" do 
+    expect(Cat.create(name: 'Luffy', age: 2, enjoys: 'Walks in the park', image: 'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0c3xlbnwwfHwwfHx8MA%3D%3D')).to be_valid
+  end
+  it "should validate name" do
+    cat = Cat.create(age: 2, enjoys: 'Walks in the park', image: 'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0c3xlbnwwfHwwfHx8MA%3D%3D')
+    expect(cat.errors[:name]).to_not be_empty
+  end
+  it "has a valid age" do
+    cat = Cat.create(name: "Luffy", enjoys: 'Walks in the park', image: 'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&
+    ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0c3xlbnwwfHwwfHx8MA%3D%3D')
+    expect(cat.errors[:age]).to include "can't be blank"
+  end
+  it "has a valid enjoys" do
+    cat = Cat.create(name: "Luffy", age: 2, image: 'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0c3xlbnwwfHwwfHx8MA%3D%3D')
+    expect(cat.errors[:enjoys]).to include "can't be blank"
+  end
+  it "has a valid image" do
+    cat = Cat.create(name: "Luffy", age: 2, enjoys: 'Walks in the park')
+    expect(cat.errors[:image]).to include "can't be blank"
+  end
+  it "has an enjoys attribute that is at least 10 characters" do
+    cat = Cat.create(name: "Straw Hat Luffy", age: 2, enjoys: 'eating', image: 'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0c3xlbnwwfHwwfHx8MA%3D%3D')
+    expect(cat.errors[:enjoys]).to include "is too short (minimum is 10 characters)"
+  end
 end
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -30,15 +30,29 @@ RSpec.describe "Cats", type: :request do
       }
       # Send the request to the server
       post '/cats', params: cat_params
-  
       # Assure that we get a success back
       expect(response).to have_http_status(200)
-  
       # Look up the cat we expect to be created in the db
       cat = Cat.first
-  
-      # Assure that the created cat has the correct attributes
-      expect(cat.name).to eq 'Buster'
+
+    # assure the cat has the correct attributes
+    expect(cat.name).to eq('Buster')
+    expect(cat.age).to eq(4)
+    expect(cat.enjoys).to eq('Meow Mix, and plenty of sunshine.')
+    expect(cat.image).to eq('https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80')
+    end
+
+    it 'returns a 422 status code' do
+      invalid_cat_params = {
+        cat: {
+          name: nil,
+          age: nil,
+          enjoys: nil,
+          image: nil
+        }
+      }
+    post '/cats', params: invalid_cat_params
+    expect(response).to have_http_status(422)
     end
   end
 
